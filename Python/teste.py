911f08ed10af10712b2be7a82bbd0f508bbf33ef
Kevyn-Santos <154701163+Kevyn-Santos@users.noreply.github.com>
2025-02-02 13:24:44 -0300

    Primeiro Arquivo de Python
    Primeiro arquivo para estudo de python, será visto a base para posteriormente passarmos para analise de daods

---
+++ Python/teste.py
@@ -0,0 +1,19 @@
+# Variaveis não precisam de palavra chave para declaração, e são fracamente tipadas, mas seu tipo pode mudar
+# Os tipos basicos de dados são Int, Float, String, Bool
+# Os operadores principais são Soma(+), Subtração(-), Multiplicação(*), Divisão(/), Módulo(Resto(%)) 
+
+
+faturamento = 1000 #Primeira declaração de uma variavel
+custo = 700
+novas_Vendas = 300
+
+faturamento = faturamento + novas_Vendas # segunda declaração de uma variavel, agoar seu valor não é mais mil, mas sim 1300. ou seja, as variaveis em python não são constantes, mas sim mutaveis.
+
+imposto = faturamento * 0.1 # 1 = 100% de algo, sendo assim, 0.1 = 10% de algo. O separador de decimais é o ponto (.)
+lucro = faturamento - custo - imposto
+margem_de_lucro = (lucro / faturamento) * 100
+
+print("O faturamento é de", faturamento)
+print("O imposto é de", imposto)
+print("O lucro é de", lucro)
+print("A margem lucro é de", margem_de_lucro, "%")
